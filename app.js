b7c2a81cb192c4c9bac944253d5b3cc59c1d24e0
Casres <christian@casillas.tech>
2022-04-14 15:12:13 -0400

    SP on lesson 1

--- app.js
+++ app.js
@@ -2,6 +2,16 @@
 
 
 
-var commandLineArgs = process.argv; 
 
-console.log(commandLineArgs);
+
+
+
+var profileDataArgs = process.argv.slice(2, process.argv.length);
+
+const printProfileData = profileDataArgs => {
+    
+    profileDataArgs.forEach(profileItem => console.log(profileItem));
+
+};
+
+printProfileData(profileDataArgs);
