49f1288571f0a58dc438da97ba9750bec55c9e94
Casres <christian@casillas.tech>
2022-04-20 11:58:45 -0400

    AskBCS SP

--- app.js
+++ app.js
@@ -1,19 +1,80 @@
 
 
 
+const inquirer = require('inquirer');
+
+const promptUser = portfolioData => {
+
+    if (!portfolioData.projects) {
+        portfolioData.projects = [];
+    }
 
 
-var fs = require('fs');
-const generatePage = require('./src/page-template');
+    console.log(`
+    =================
+    Add a New Project
+    =================
+`);
 
-// input of the user from the command line
-var profileDataArgs = process.argv.slice(2);
+    return inquirer.prompt([
+        {
+            type: 'input', 
+            name: 'name',
+            message: 'What is the name of your project?'
+        },  
+        {
+            type: 'input', 
+            name: 'description',
+            message: 'Provide a description of your project (required)'
+        },
+        {
+            type: 'checkbox', 
+            name: 'languages',
+            message: 'What did you build this project with? (Check all that apply)',
+            choices: ['JavaScript', 'HTML', 'CSS', 'jQuery', 'BootStrap', 'Node', 'Other']
+        },
+        {
+            type: 'input', 
+            name: 'link',
+            message: 'GitHub project link (Required?'
+        },  
+        {
+            type: 'confirm', 
+            name: 'feature',
+            message: 'What would you like to feature in this project?',
+            default: false
+        },
+        {
+            type: 'confirm', 
+            name: 'confirmAddProject',
+            message: 'Would you like to enter another project?',
+            default: false
+        }
+    ])
+    .then(projectData => {
+        portfolioData.projects.push(projectData);
+        if (projectData.confirmAddProject) {
+            return promptProject(portfolioData);
+        } else {
+            return portfolioData;
+        }
+    });
+}
 
-// putting user command line inputs into variable names 'name' , 'github'
-const [name, github] = profileDataArgs;
+promptUser()
+  .then(answers => console.log(answers))
+//   .then(promptProject)
+//   .then(projectAnswers => console.log(projectAnswers));
 
-// putting the user inputs that are now rep by variables into the generate page var who's value is an html with inputs that are waiting to be populated 
-const pageHTML = generatePage(name, github);
+// var fs = require('fs');
+// const generatePage = require('./src/page-template');
 
-// puts it all together by writing a file and then plugging in the information for that file and then putting a call back function for errors
-fs.writeFile('index.html', pageHTML, error => error ? console.log('error has happened') : console.log('Portfolio complete! Go to index.html to see the work!'));
+// // input of the user from the command line
+
+// // putting user command line inputs into variable names 'name' , 'github'
+
+// // putting the user inputs that are now rep by variables into the generate page var who's value is an html with inputs that are waiting to be populated 
+// const pageHTML = generatePage(name, github);
+
+// // puts it all together by writing a file and then plugging in the information for that file and then putting a call back function for errors
+// fs.writeFile('index.html', pageHTML, error => error ? console.log('error has happened') : console.log('Portfolio complete! Go to index.html to see the work!'));
